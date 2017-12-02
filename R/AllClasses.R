a4980d58307969a6151b50bc513a35933d22be24
Bernhard Pfaff <bernhard@pfaffikus.de>
2017-12-02 15:26:05 +0100

    Added conrpc() function and CONRPC S4-object.

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -2,18 +2,20 @@
 #'
 #' S4-class for curl connections to RPC-JSON. 
 #'
-#' The slots \code{rpcuser} and \code{rpcpassowrd} are required in the call
-#' to \code{curl}. Furthermore, the fully qualified path to bitcoin.conf is
-#' required for starting and stopping bitcoind as daemon.
+#' The slots \code{rpcuse} and \code{rpcpwd} are required in the call
+#' to \code{curl}. Furthermore, the fully qualified path to
+#' \code{bitcoin.conf} (slot \code{config}) is required for starting
+#' and stopping \code{bitcoind} as daemon. 
 #'
+#' @seealso conrpc, startbtc, stopbtc
 #' @name CONRPC 
-#' @rdname CONRPC
+#' @rdname CONRPC-class
 #' @aliases CONRPC-class
 #' @exportClass CONRPC
 #' @author Bernhard Pfaff
 #'
 setClass("CONRPC", representation = list(
-                       rpcuser = "character",
+                       rpcuse = "character",
                        rpcpwd = "character",
-                       conf.file = "character")
+                       config = "character")
          )
--- R/bitcoind.R
+++ R/bitcoind.R
@@ -1,32 +1,72 @@
+#' Extracting Configuration Settings
+#'
+#' This function extracts information from the configuration
+#' file \code{bitcoin.conf} with respect to the options  \code{rpcuser}
+#' and \code{rpcpassword}.
+#'
+#' @param conf.file \code{character}, the fully qualified path.
+#'
+#' @return An S4-object of class \code{CONRPC}.
+#' @seealso CONRPC-class, startbtc, stopbtc
+#' @author Bernhard Pfaff
+#' @name conrpc
+#' @aliases conrpc 
+#' @rdname conrpc
+#' @export
+#' 
+conrpc <- function(conf.file){
+    con <- file(conf.file, open = "r", blocking = FALSE)
+    bconf <- readLines(con)
+    close(con)
+    bconfl <- strsplit(bconf, split = "=")
+    idx <- which(unlist(lapply(bconfl, function(x) x[1] == "rpcuser")))
+    if (length(idx) > 0){
+        valrpcuse <- bconfl[[idx]][2]
+    } else {
+        warning("'rpcuser' not set, using 'rbtc'.\n")
+        valrpcuse <- "rbtc"
+    }
+    idx <- which(unlist(lapply(bconfl, function(x) x[1] == "rpcpassword")))
+    if (length(idx) > 0){
+        valrpcpwd <- bconfl[[idx]][2]
+    } else {
+        warning("'rpcpassword' not set, creating random string.\n")
+        valrpcpwd <- paste0(sample(LETTERS, 12, TRUE), collapse = "")
+    }
+    new("CONRPC",
+        rpcuse = valrpcuse,
+        rpcpwd = valrpcpwd,
+        config = conf.file)
+}
 #' Start bitcoind server process
 #'
 #' This function does start the bitcoind-server process.
 #' It should only be called when no suitable RPC-JSON
 #' process is running
 #'
-#' @param rpcuser \code{character}, the name of the RPC-user.
-#' @param rpcpassword \code{character}, a given (strong) password.
-#' @param conf.file \code{character}, the path and filename to the
-#' bitcoind configuration file.
+#' @param confbtc \code{CONRPC} object, returned from \code{conrpc()}.
 #'
-#' @details The default value for \code{conf.file} is \code{NULL}.
-#' In this case, the bitcoind process is started with the options:
-#' server, rpcuser and rpcpassword. The process is started by calling
-#' \code{system2()}.
+#' @details The process is started by calling \code{system()}.
+#' Hereby, the options: \code{rpcuser}, \code{rpcpassword} and
+#' \code{conf} are used in the call to \code{bitcoind}.
 #'
+#' @seealso conrpc, CONRPC-class, stopbtc
 #' @author Bernhard Pfaff
 #' @return \code{NULL}
+#' @name startbtc
+#' @aliases startbtc 
+#' @rdname startbtc
 #' @export
 #' 
-startbtc <- function(rpcuser = "rbtc", rpcpassword = "$GiveItATry4Me",
-                     conf.file = NULL){
-    if (is.null(conf.file)){
-        strcmd <- paste("bitcoind -daemon -rpcuser=", rpcuser,
-                        " -rpcpassword=", rpcpassword, sep = "")
-    } else {
-        strcmd <- paste("bitcoind -daemon -rpcuserconf=", conf.file,
-                        sep = "")
-    }
+startbtc <- function(confbtc){
+    stopifnot(class(confbtc) == "CONRPC")
+    rpcuse <- slot(confbtc, "rpcuse")
+    rpcpwd <- slot(confbtc, "rpcpwd")
+    config <- slot(confbtc, "config")
+    strcmd <- paste("bitcoind -daemon -rpcuser=", rpcuse,
+                    " -rpcpassword=", rpcpwd,
+                    " -conf=", config,
+                    sep = "")
     system(strcmd)
     NULL
 }
@@ -34,27 +74,27 @@ startbtc <- function(rpcuser = "rbtc", rpcpassword = "$GiveItATry4Me",
 #'
 #' This function stops a running bitcoind process.
 #' It calls \code{bitcoin-cli stop} \emph{via} the
-#' R function \code{system2()}.
+#' R function \code{system()}.
 #'
-#' @param rpcuser \code{character}, the name of the RPC-user.
-#' @param rpcpassword \code{character}, a given (strong) password.
-#' @param conf.file \code{character}, the path and filename to the
-#' bitcoind configuration file.
+#' @param confbtc \code{CONRPC} object, returned from \code{conrpc()}.
 #' 
 #' @return NULL
+#' @seealso startbtc, conrpc, CONRPC-class
 #' @author Bernhard Pfaff
+#' @name stopbtc
+#' @aliases stopbtc 
+#' @rdname stopbtc
 #' @export
 #'
-stopbtc <- function(rpcuser = "rbtc", rpcpassword = "$GiveItATry4Me",
-                     conf.file = NULL){
-    if (is.null(conf.file)){
-        strcmd <- paste("bitcoin-cli -rpcuser=", rpcuser,
-                        " -rpcpassword=", rpcpassword,
-                        " stop", sep = "")
-    } else {
-        strcmd <- paste("bitcoin-cli -rpcuserconf=", conf.file,
-                        " stop", sep = "")
-    }
+stopbtc <- function(confbtc){
+    stopifnot(class(confbtc) == "CONRPC")
+    rpcuse <- slot(confbtc, "rpcuse")
+    rpcpwd <- slot(confbtc, "rpcpwd")
+    config <- slot(confbtc, "config")
+    strcmd <- paste("bitcoin-cli -rpcuser=", rpcuse,
+                    " -rpcpassword=", rpcpwd,
+                    " -conf=", config,
+                    " stop", sep = "")
     system(strcmd)
     NULL
 }
