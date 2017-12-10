c2bd390e6e2b7de966e44e756771c7da742ef48b
Bernhard Pfaff <bernhard@pfaffikus.de>
2017-12-10 21:13:25 +0100

    Moved getinfo() and gethelp() to new R file.

--- R/ControlRpcs.R
+++ R/ControlRpcs.R
@@ -29,3 +29,32 @@ getinfo <- function(conobj){
                 encode = "json")
     content(ans)
 }
+#' RPC-JSON API: Help
+#'
+#' Returning information about RPC functions.
+#'
+#' @param conobj object of class \code{CONRPC}.
+#' @param rpcname \code{character}, name of RPC function.
+#'
+#' @return A coerced \code{list} object from RPC-JSON API.
+#' @family Control RPCs
+#' @author Bernhard Pfaff
+#' @references \url{https://bitcoin.org/en/developer-reference#remote-procedure-calls-rpcs}
+#' @name gethelp
+#' @aliases gethelp 
+#' @rdname gethelp
+#' @export
+gethelp <- function(conobj, rpcname = ""){
+    stopifnot(class(conobj) == "CONRPC")
+    rpcname <- as.character(rpcname)[1]
+    ans <- POST(slot(conobj, "url"),
+                authenticate(user = slot(conobj, "rpcuse"),
+                             password = slot(conobj, "rpcpwd"),
+                             type = "basic"),
+                body = list(jsonrpc = "1.0",
+                            id = "curltest",
+                            method = "help",
+                            params = list(command = rpcname)),
+                encode = "json")
+    content(ans)
+}
